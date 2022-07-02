277d9dd001d779b796b6c9969682e2ae79ec0f4b
gsycan <gsycan@163.com>
2022-07-02 22:18:10 +0800

    init project

---
+++ src/main/java/com/gsycan/catcoder/CatcoderApplication.java
@@ -0,0 +1,13 @@
+package com.gsycan.catcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatcoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatcoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsycan/catcoder/CatcoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsycan.catcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatcoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
